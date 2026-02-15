d28261a1e0a5f56be7a32bd009addfdc1201301b
Kostiantyn Teltov <dneprokos@gmail.com>
2026-02-15 09:01:33 +0100

    added monocart report sample

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,19 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: [
+    ["list"],
+    [
+      "monocart-reporter",
+      {
+        name: "Monocart Test Report",
+        outputFile: "./monocart-report/index.html",
+        tree: {
+          file: false,
+        },
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
